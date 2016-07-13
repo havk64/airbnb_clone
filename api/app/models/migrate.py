501aa9658822e81ef935ef125f89027526696320
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-07-12 22:12:22 -0300

    Added migrate.py

---
+++ api/app/models/migrate.py
@@ -0,0 +1,12 @@
+from base import db
+from user import User
+from state import State
+from city import City
+from place import Place
+from place_book import PlaceBook
+from amenity import Amenity
+from place_amenity import PlaceAmenities
+
+db.connect()
+
+db.create_tables([User, State, City, Place, PlaceBook, Amenity, PlaceAmenities], safe = True)
