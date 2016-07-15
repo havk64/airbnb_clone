6af638d6096fbc9a6947d53131ffb6222480ecef
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-07-14 23:39:41 -0300

    Adding Test option to config file, task 1

--- api/config.py
+++ api/config.py
@@ -1,25 +1,55 @@
 """
- ===----Task 2------------------------------------------------------------------------===
-	Airbnb Clone Project, Task 2.
-	Isolating environments in Python: Development/Production.
+ ===----Config------------------------------------------------------------------------===
+	Airbnb Clone Project, config file.
+	Isolating environments in Python: Development/Production/Test.
  ===----------------------------------------------------------------------------------===
 """
-import os
+from os import environ
 
-ENV = os.environ
+ENV = environ
 if 'AIRBNB_ENV' not in ENV:
     raise Exception("Environment variable AIRBNB_ENV not set")
 
-test = True if ENV['AIRBNB_ENV'] == 'production' else False
+def setenv(env):
+    options = {}
+    if env['AIRBNB_ENV'] == 'production':
+        options['debug']    = False
+        options['host']     = '0.0.0.0'
+        options['port']     = 3000
+        options['user']     = 'airbnb_user_prod'
+        options['db']       = 'airbnb_prod'
+        options['password'] = ENV.get('AIRBNB_DATABASE_PWD_PROD')
+    elif env['AIRBNB_ENV'] == 'development':
+        options['debug']    = True
+        options['host']     = 'localhost'
+        options['port']     = 3333
+        options['user']     = 'airbnb_user_dev'
+        options['db']       = 'airbnb_dev'
+        options['password'] = ENV.get('AIRBNB_DATABASE_PWD_DEV')
+    else:
+        options['debug']    = False
+        options['host']     = 'localhost'
+        options['port']     = 5555
+        options['user']     = 'airbnb_user_test'
+        options['db']       = 'airbnb_test'
+        options['password'] = ENV.get('AIRBNB_DATABASE_PWD_TEST')
 
-DEBUG = (True, False)[test]
-HOST = ('localhost', '0.0.0.0')[test]
-PORT = (3333, 3000)[test]
+    return options
+
+
+options = setenv(ENV)
+
+DEBUG   = options['debug']
+HOST    = options['host']
+PORT    = options['port']
 DATABASE = {
     'host': '158.69.78.253',
-    'user': ( 'airbnb_user_dev', 'airbnb_user_prod')[test],
-    'database': ('airbnb_dev', 'airbnb_prod')[test],
+    'user': options['user'],
+    'database': options['db'],
     'port': '3306',
     'charset': 'utf8',
-    'password': (ENV.get('AIRBNB_DATABASE_PWD_DEV'), ENV.get('AIRBNB_DATABASE_PWD_PROD'))[test]
+    'password': options['password']
 }
+
+print(DEBUG, HOST, PORT)
+print(DATABASE)
