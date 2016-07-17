3961ba00ecf2cc25215c4e01f66fd0d22b929e6f
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-07-16 22:44:02 -0300

    Optimizing base model

--- api/app/models/base.py
+++ api/app/models/base.py
@@ -2,19 +2,27 @@ from peewee import *
 from config import *
 from datetime import datetime
 
-database = MySQLDatabase(DATABASE.get("database"),
-                   host=DATABASE.get("host"),
-                   port=DATABASE.get("port"),
-                   user=DATABASE.get("user"),
-                   passwd=DATABASE.get("password"))
+database = MySQLDatabase(host       = DATABASE["host"],
+                         port       = DATABASE["port"],
+                         user       = DATABASE["user"],
+                         password   = DATABASE["password"],
+                         database   = DATABASE["database"]) 
 
 class BaseModel(Model):
-    id = PrimaryKeyField(primary_key=True, unique=True)
+    id = PrimaryKeyField(unique=True)
     created_at = DateTimeField(default=datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
     updated_at = DateTimeField(default=datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
 
     def save(self, *args, **kwargs):
-        self.update_at = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        self.update_at = datetime.now()
+        super(BaseModel, self).save()
+
+    def to_hash(model, self, data):
+        ''' Returns a hash of the BaseModel in the database '''
+        data['id'] = self.id
+        data['created_at'] = self.created_at.strftime("%d/%m/%Y %H:%M:%S")
+        data['updated_at'] = self.updated_at.strftime("%d/%m/%Y %H:%M:%S")
+        return data
 
     class Meta:
         database = database
