327ad064179856978a84e9316447e9b1bf494c4c
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-07-16 22:41:36 -0300

    Minor fixes

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_json import FlaskJSON
 
 app = Flask(__name__)
+app.config['JSON_ADD_STATUS'] = False
+
 json = FlaskJSON(app)
 
-from app.views import *
+from views import *
