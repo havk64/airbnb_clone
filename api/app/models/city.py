64338ebbd3341b46aabfcec146de2335a8b24c12
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-07-24 20:21:07 -0300

    Added comments, fixes

--- api/app/models/city.py
+++ api/app/models/city.py
@@ -3,9 +3,13 @@ from base   import BaseModel
 from state  import State
 
 class City(BaseModel):
+    """Definition of City Model"""
     name = CharField(128, null = False)
     state = ForeignKeyField(State, related_name='cities', on_delete='CASCADE')
+
     def to_hash():
+        """Method to_hash returns City object model to hash"""
+        state = State.get(State.id == self.state)
         city = {
             'name'      : self.name,
             'state_id'  : state.id,
