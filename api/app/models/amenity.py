d071c724b2fff2b5419b2c90e97f7325d6e80324
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-07-24 19:20:36 -0300

    Minor fix

--- api/app/models/amenity.py
+++ api/app/models/amenity.py
@@ -2,7 +2,7 @@ from base import BaseModel
 from peewee import CharField
 
 class Amenity(BaseModel):
-    """Definition os Amenity Objct Model"""
+    """Definition os Amenity Object Model"""
     name = CharField(128, null = False)
 
     def to_hash(self):
