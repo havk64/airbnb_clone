3cee61efbcb6e80cbb7a192f6570f7d6accf0c2e
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-07-24 17:19:55 -0300

    Adding comments

--- api/app/models/state.py
+++ api/app/models/state.py
@@ -2,8 +2,11 @@ from peewee import CharField
 from base import BaseModel
 
 class State(BaseModel):
+    """State class definition"""
     name = CharField(128, null = False, unique = True)
+
     def to_hash():
+        """Function to return the hash of State object model"""
         place = {
 				'name': self.name
         }
