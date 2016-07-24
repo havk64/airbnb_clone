86a44cfa4c1468f7cd4936b89fe425d380f35d50
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-07-24 17:29:01 -0300

    Added more comments

--- api/app/models/state.py
+++ api/app/models/state.py
@@ -6,8 +6,8 @@ class State(BaseModel):
     name = CharField(128, null = False, unique = True)
 
     def to_hash():
-        """Function to return the hash of State object model"""
-        place = {
+        """Method to return the hash of State object model"""
+        state = {
 				'name': self.name
         }
-        return super(State, self).to_hash(self, place)
+        return super(State, self).to_hash(self, state)
