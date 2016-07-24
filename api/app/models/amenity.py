a49606527c62605aa02bf80170619863388983fd
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-07-24 19:16:46 -0300

    Added comments

--- api/app/models/amenity.py
+++ api/app/models/amenity.py
@@ -2,9 +2,11 @@ from base import BaseModel
 from peewee import CharField
 
 class Amenity(BaseModel):
+    """Definition os Amenity Objct Model"""
     name = CharField(128, null = False)
 
     def to_hash(self):
+        """Method to_hash returns the Amenity Object model to hash"""
         amenity = {
             'name'  = self.name,
         }
