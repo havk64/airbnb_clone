71ad6df64bc7056890cacee82fd6fd5c6dadd42f
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-07-24 19:15:40 -0300

    Added method to_hash to Amenity model

--- api/app/models/amenity.py
+++ api/app/models/amenity.py
@@ -3,3 +3,9 @@ from peewee import CharField
 
 class Amenity(BaseModel):
     name = CharField(128, null = False)
+
+    def to_hash(self):
+        amenity = {
+            'name'  = self.name,
+        }
+        return super(Amenity, self).to_hash(self, amenity)
