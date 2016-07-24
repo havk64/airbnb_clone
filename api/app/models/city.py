86b43995ab1499c070ee95cf1b235f37088fa4f2
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-07-24 18:26:07 -0300

    Added to_hash to city model

--- api/app/models/city.py
+++ api/app/models/city.py
@@ -5,3 +5,9 @@ from state  import State
 class City(BaseModel):
     name = CharField(128, null = False)
     state = ForeignKeyField(State, related_name='cities', on_delete='CASCADE')
+    def to_hash():
+        city = {
+            'name'      : self.name,
+            'state_id'  : state.id,
+        }
+        return super(City, self).to_hash(self, city)
