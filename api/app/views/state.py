303261eb86979fd5f3d403d41c8cd3ca66df20d5
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-07-24 22:34:30 -0300

    Added function to receive Post req and create a State

--- api/app/views/state.py
+++ api/app/views/state.py
@@ -10,3 +10,15 @@ def get_states():
     for i in query:
         states.append(i.to_hash)
     return jsonify(states), 200
+
+@app.route('/states', methods = ['POST'])
+@as_json
+def create state():
+    data = request.form
+    name_check = State.select().where(State.name = data['name'])
+    if name_check:
+        return {'code': 10000, 'msg': 'State already exists'}, 409
+    state = State.create(
+        name = data['name']
+    )
+    return {'code': 201,'msg': 'State was created successfully'}, 201
