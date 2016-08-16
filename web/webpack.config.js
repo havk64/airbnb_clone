c343fdf60959e2834badae862c60bf5eef973241
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-08-15 22:41:10 -0300

    Task 0 completed

---
+++ web/webpack.config.js
@@ -0,0 +1,24 @@
+var webpack	= require('webpack'),
+    path	= require('path');
+
+module.exports = {
+	debug: true,
+	entry: {
+		main: './js/app.js'
+	},
+	output: {
+		path: __dirname,
+		filename: 'static/bundle.js'
+	},
+	module: {
+		loaders: [
+		{
+			loader: 'babel-loader',
+			exclude: '/node_modules/',
+			query: {
+				presets: ['es2015', 'react']
+			}
+		}
+		]
+	}
+};
