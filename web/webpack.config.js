c26b75e047b2d91b94cfdceb1300321f0058f288
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-08-17 20:31:45 -0300

    Minor change

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
 	module: {
 		loaders: [{
 			test	: /\.jsx?$/,
-			loader	: 'babel',
+			loader	: 'babel-loader',
 			exclude	: /node_modules/,
 			query	: {
 				presets: ['es2015', 'react']
