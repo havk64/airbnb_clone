c358aab087dd527d6ba172f200220dd5e8c0aacd
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-08-18 03:02:02 -0300

    Updated app with Left Column component

--- web/js/app.js
+++ web/js/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Components/Header.js';
+import LeftColumn from './Components/LeftColumn.js';
 
 ReactDOM.render(
 		<div>
 		<Header />
+		<LeftColumn />
 		</div>,
 		document.getElementById('container')
 	       );
