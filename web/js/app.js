3290dde264b256cb51101420b53a4c647fa4d740
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-08-18 20:05:27 -0300

    Added Content, Footer to main app

--- web/js/app.js
+++ web/js/app.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Components/Header.js';
 import LeftColumn from './Components/LeftColumn.js';
+import Content from './Components/Content.js';
+import Footer from './Components/Footer.js';
+
+const divStyle = { height: "100%", border: "1px solid yellow" };
 
 ReactDOM.render(
-		<div>
-		<Header />
-		<LeftColumn />
+		<div style={divStyle}>
+			<Header />
+			<main>
+				<LeftColumn />
+				<Content />
+			</main>
+			<Footer />
 		</div>,
 		document.getElementById('container')
 	       );
