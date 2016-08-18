d0909f2459e1a4bb1349dbc28dfd3feb870e2f9f
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-08-17 21:12:37 -0300

    Added header to app.js

--- web/js/app.js
+++ web/js/app.js
@@ -1,1 +1,10 @@
-console.log("Hello, React!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Components/Header.js';
+
+ReactDOM.render(
+		<div>
+		<Header />
+		</div>,
+		document.getElementById('container')
+	       );
