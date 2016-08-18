35fc45dc5824f98162d7219c1a99c889aa594059
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-08-18 20:01:12 -0300

    Created Content as React Component

---
+++ web/js/Components/Content.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Content = React.createClass({
+	render() {
+		const contentStyle = {
+			height	: "100%",
+			float	: "left"
+		}
+		return (
+			<div style={contentStyle}>
+			</div>
+		       );
+	}
+});
+
+export default Content;
