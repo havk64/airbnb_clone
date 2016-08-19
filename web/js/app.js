97f0d3b691ffa58a5d00fb8229c9780772f6e7ba
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-08-18 22:00:20 -0300

    Indentation

--- web/js/app.js
+++ web/js/app.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from './Components/Header.js';
-import LeftColumn from './Components/LeftColumn.js';
-import Content from './Components/Content.js';
-import Footer from './Components/Footer.js';
+import React 		from 'react';
+import ReactDOM		from 'react-dom';
+import Header		from './Components/Header.js';
+import LeftColumn	from './Components/LeftColumn.js';
+import Content		from './Components/Content.js';
+import Footer		from './Components/Footer.js';
 
 const divStyle 	= { height: "100%" };
 const mainStyle	= { height: "100%", marginBottom: "40px" };
+
 ReactDOM.render(
 		<div style={divStyle}>
 			<Header />
