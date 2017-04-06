b45f0845e312ab9d481013715a8db3bfb9fdca01
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-04-06 16:56:55 -0300

    Drying out the test statements by transfering variables to setup method

--- api/tests/index.py
+++ api/tests/index.py
@@ -5,26 +5,23 @@ import unittest, json
 class IndexTestCase(unittest.TestCase):
 	def setUp(self):
 		self.app = app.test_client()
+		self.resp = self.app.get('/')
 
 	def test_200(self):
-		resp = self.app.get('/')
-		self.assertEquals(resp.status_code, 200)
+		self.assertEquals(self.resp.status_code, 200)
 
 	def test_status(self):
-		resp = self.app.get('/')
-		jsonified = json.loads(resp.data)
+		jsonified = json.loads(self.resp.data)
 		self.assertEqual(jsonified['status'], "OK")
 
 	def test_time(self):
-		resp = self.app.get('/')
 		datetimenow = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-		jsonified = json.loads(resp.data)
+		jsonified = json.loads(self.resp.data)
 		resp_time = str(jsonified['time'])
 		self.assertEqual(resp_time, datetimenow)
 
 	def test_time_utc(self):
-		resp = self.app.get('/')
 		utcdatetimenow = datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S")
-		jsonified = json.loads(resp.data)
+		jsonified = json.loads(self.resp.data)
 		resp_time = str(jsonified['utc_time'])
 		self.assertEqual(resp_time, utcdatetimenow)
