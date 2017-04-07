882eb8442c1d0fa1df871a0f0665209cc5750b8f
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-04-07 18:43:30 -0300

    Import statement

--- api/app.py
+++ api/app.py
@@ -1,6 +1,5 @@
 from app import app
-from config import HOST, PORT, DEBUG
-
+from config import *
 if __name__ == '__main__' :
     ''' Initializing the app'''
     app.run(host=HOST, port=PORT, debug=DEBUG)
