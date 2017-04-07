7e22d70001fe47600413256c29a254c4ca250bb1
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-04-07 18:46:37 -0300

    Remove unnecessary import statement

--- api/app/views/place.py
+++ api/app/views/place.py
@@ -4,7 +4,6 @@ from app.models.state import State
 from app import app
 from flask import Flask, request, jsonify
 from flask_json import FlaskJSON, as_json
-from index import before_request, after_request
 
 @app.route("/places", methods=["GET"])
 def get_places():
