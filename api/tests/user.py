feab8ce342b8892669e99250e444879eda2e3433
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-04-06 23:08:50 -0300

    Refactoring to dry out, respecting just 80 columns in width

--- api/tests/user.py
+++ api/tests/user.py
@@ -1,8 +1,8 @@
-from app import app
 from datetime import datetime
 import unittest, json, logging
 from app.models.base import database
 from app.models.user import User
+from app import app
 
 class UserTestCase(unittest.TestCase):
 	def setUp(self):
@@ -16,30 +16,37 @@ class UserTestCase(unittest.TestCase):
 	def errormsg(self, expec, *got):
 		return 'Expecting {} but got {}'.format(expec, got)
 
+	def create_user(self, user, expec):
+		resp = self.app.post('/users', data=user)
+		assert resp.status == expec, self.errormsg(expec, resp.status)
+		return User.select().order_by(User.id.desc()).get()
+
+
 	def test_create(self):
 		users = [
-			{'first_name': 'Alexandro','last_name': 'de Oliveira', 'email': 'alexandro.oliveira@holbertonschool.com','password': '123', 'is_admin': True},
-			{'first_name': 'Tony', 'last_name': 'Stark', 'email': 'tony@stark.com', 'password':'456', 'is_admin': False}
-		]
+			{'first_name':'Alexandro','last_name':'de Oliveira',
+                            'email':'alexandro.oliveira@holbertonschool.com',
+                            'password':'123','is_admin':True},
+			{'first_name':'Tony','last_name':'Stark',
+                            'email':'tony@stark.com','password':'456',
+                            'is_admin': False}]
 
-		without_email = {'first_name': 'Jon', 'last_name': 'Snow', 'password': '321', 'is_admin': False}
-		dupl_email = {'first_name': 'Cris','last_name': 'Lamarc', 'email': 'alexandro.oliveira@holbertonschool.com','password': '654', 'is_admin': True}
+		without_email = {'first_name': 'Jon', 'last_name': 'Snow',
+                        'password': '321', 'is_admin': False}
+		dupl_email = {'first_name': 'Cris','last_name': 'Lamarc',
+                        'email': 'alexandro.oliveira@holbertonschool.com',
+                        'password': '654', 'is_admin': True}
 
 		count = 1
 		for user in users:
-			resp = self.app.post('/users', data=user)
-			assert resp.status_code == 201, self.errormsg(201, resp.status_code)
-			assert resp.status == '201 CREATED', self.errormsg('201 CREATED', resp.status)
-			last_user = User.select().order_by(User.id.desc()).get()
+			last_user = self.create_user(user, '201 CREATED')
 			assert last_user.id == count, self.errormsg(count, last_user.id)
 			count += 1
 
-		resp = self.app.post('/users', data=without_email)
-		assert resp.status == '400 BAD REQUEST', self.errormsg('400 BAD REQUEST', resp.status)
-		last_user = User.select().order_by(User.id.desc()).get()
-		assert last_user.email == 'tony@stark.com', self.errormsg('tony@stark.com', str(last_user.email))
+		last_user = self.create_user(without_email, '400 BAD REQUEST')
+		assert last_user.email == 'tony@stark.com',\
+		self.errormsg('tony@stark.com', str(last_user.email))
 
-		resp = self.app.post('/users', data=dupl_email)
-		assert resp.status == '409 CONFLICT', self.errormsg('409 CONFLICT', resp.status)
-		last_user = User.select().order_by(User.id.desc()).get()
-		assert last_user.email == 'tony@stark.com', self.errormsg('tony@stark.com', str(last_user.email))
+		last_user = self.create_user(dupl_email, '409 CONFLICT')
+		assert last_user.email == 'tony@stark.com',\
+		self.errormsg('tony@stark.com', str(last_user.email))
