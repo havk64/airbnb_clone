864578c2849e4cc4e314e1e4d64df3bb44aaea6f
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-04-08 17:54:03 -0300

    Transfering check and create_row functions to the base class

--- api/tests/base.py
+++ api/tests/base.py
@@ -10,7 +10,14 @@ class BaseTestCase(unittest.TestCase):
 
     def tearDown(self):
         database.drop_table(self.table)
-	# Auxiliary function to return error message for assert function
 
     def errormsg(self, expec, *got):
         return 'Expecting {} but got {}'.format(expec, got)
+
+    def check(self, value, expected):
+        assert value == expected, self.errormsg(expected, value)
+
+    def create_row(self, data, expec):
+        resp = self.app.post(self.path, data=data)
+        self.check(resp.status, expec)
+        return self.table.select().order_by(self.table.id.desc()).get()
