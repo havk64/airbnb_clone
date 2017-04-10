7a07bb7dfa557b587eb1dd17bf7829270be63df0
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-04-09 22:31:50 -0300

    Code cleanup, variable names

--- api/tests/user.py
+++ api/tests/user.py
@@ -16,21 +16,17 @@ class UserTestCase(BaseTestCase):
 			self.check(last_user.id, count)
 			count += 1
 		# It should return bad request when email is not given.
-		last_user = self.create_row(without_email, '400 BAD REQUEST')
-		assert last_user.email == 'jon@snow.com',\
-		self.errormsg('jon@snow.com', str(last_user.email)) # user not created
+		last_user = self.create_row(user_bad_req, '400 BAD REQUEST')
+		self.check(last_user.email, 'jon@snow.com') # user not created
 		# It should return code 10001 when trying to create user with duplicated email
 		last_user = self.check_dupl_entry(dupl_email, 10000)
-		assert last_user.email == 'jon@snow.com',\
-		self.errormsg('jon@snow.com', str(last_user.email))
-		# It should return code 10000 when user's email is duplicated
+		self.check(last_user.email, 'jon@snow.com')
 
 	def test_list(self):
 		self.check_list()
 
 	def test_get(self):
 		self.check_get('User')
-        # assert last_user.email == self.users[0]['email'], self.errormsg(self.users['email'], last_user.email)
 
 	def test_delete(self):
 		self.check_delete('User')
@@ -38,7 +34,6 @@ class UserTestCase(BaseTestCase):
 	def test_update(self):
 		upd_data = {'first_name':'George', 'last_name':'Harrison'}
 		self.check_update(upd_data)
-
 		# It should give internal server error when trying to change email
 		resp = self.app.put('{}/{}'.format(self.path, 1), data={'email':'new@email.com'})
 		self.check(resp.status_code, 500)
