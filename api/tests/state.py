b3f1d0178a0f832cb27d1bc60c3fe3579cbf3c4d
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-04-09 22:55:53 -0300

    Code cleanup, using predefined functions

--- api/tests/state.py
+++ api/tests/state.py
@@ -13,14 +13,14 @@ class StateTestCase(BaseTestCase):
 		count = 1
 		for state in states:
 			last_state = self.create_row(state, '201 CREATED')
-			assert last_state.id == count, self.errormsg(count, last_state.id)
-			assert last_state.name == state['name'], self.errormsg(state['name'], last_state.id)
+			self.check(last_state.id, count)
+			self.check(last_state.name, state['name'])
 			count += 1
 		# It should return code 10001 when trying to create duplicated state name.
 		dupl_state = {'name': 'California'}
 		last_state = self.check_dupl_entry(dupl_state, 10001)
 		# The last entry on database should be the previous one
-		assert last_state.name == 'District of Columbia', self.errormsg('District of Columbia',last_state.name)
+		self.check(last_state.name, 'District of Columbia')
 
 	def test_list(self):
 		self.check_list()
