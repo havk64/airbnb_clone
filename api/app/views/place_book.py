8b5583cabaee07e570d24998b057cd60f0801701
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-04-11 21:53:41 -0300

    Add place_book to views

---
+++ api/app/views/place_book.py
@@ -0,0 +1,67 @@
+from app import app
+from app.models.place_book import PlaceBook
+from app.models.place import Place
+from flask_json import as_json, request, jsonify
+from datetime import datetime
+
+@app.route('/places/<int:id>/books', methods=['GET'])
+@as_json
+def get_place_books(id):
+    books = []
+    query = PlaceBook.select().join(Place).where(Place.id == id)
+    for book in query:
+        books.append(book.to_hash())
+    return jsonify(books)
+
+@app.route('/places/<int:id>/books', methods=['POST'])
+@as_json
+def create_books(id):
+    data = request.form
+    book = PlaceBook()
+    book.place = id
+    for entry in data:
+        if entry == 'date_start':
+            setattr(book, entry, datetime.strptime(data[entry], '%Y/%m/%d %H:%M:%S'))
+        else:
+            setattr(book, entry, data[entry])
+
+    book.save()
+    return {'code': 201, 'msg': 'Book created successfully'}, 201
+
+@app.route('/places/<int:pid>/books/<int:id>', methods=['GET'])
+@as_json
+def get_book(sid, id):
+    try:
+        book = PlaceBook.get(PlaceBook.id == id, PlaceBook.place_id == sid)
+    except Exception:
+        return {'code': 404, 'msg': 'Book not found'}, 404
+
+    return book.to_hash(), 200
+
+@app.route('/places/<int:pid>/books/<int:id>', methods=['DELETE'])
+@as_json
+def delete_book(sid, id):
+    try:
+        book = PlaceBook.get(PlaceBook.id == id, PlaceBook.place_id == sid)
+    except Exception:
+        return {'code': 404, 'msg': 'Book not found'}, 404
+    book.delete_instance()
+    return {'code':200, 'msg': 'Deleted successfully'}, 200
+
+@app.route('/places/<int:sid>/books/<int:id>', methods=['PUT'])
+@as_json
+def update_books(sid, id):
+    data = request.form
+    try:
+        book = PlaceBook.get(PlaceBook.id == id, PlaceBook.place_id == sid)
+    except Exception:
+        return {'code': 404, 'msg': 'Book not found'}, 404
+    for i in data:
+        if i == 'user':
+            raise Exception("User can't be changed")
+        if i == 'place':
+            setattr(book, 'place_id', data[i])
+        if i == 'date_start':
+            setattr(book, i, datetime.strptime(data[i], '%Y/%m/%d %H:%M:%S'))
+        else:
+            setattr(book, i, data[i])
