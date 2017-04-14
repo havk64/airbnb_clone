41fda806d9cd2d3f3a18b4ed779d9b66f825165e
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-04-13 23:22:08 -0300

    Update migrate with 3 new tables

--- api/migrate.py
+++ api/migrate.py
@@ -6,7 +6,10 @@ from app.models.place import Place
 from app.models.place_book import PlaceBook
 from app.models.amenity import Amenity
 from app.models.place_amenity import PlaceAmenities
+from app.models.review import Review
+from app.models.review_user import ReviewUser
+from app.models.review_place import ReviewPlace
 
 database.connect()
-database.create_tables([User, State, City, Place, Amenity, PlaceBook, PlaceAmenities], safe = True)
+database.create_tables([User, State, City, Place, Amenity, PlaceBook, PlaceAmenities, Review, ReviewUser, ReviewPlace], safe = True)
 database.close()
