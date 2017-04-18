e15f4362bfabfbab5ea3b5f6f23063f8babc51a8
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-04-18 15:44:01 -0300

    Fix typo, indentation

--- api/app/models/review.py
+++ api/app/models/review.py
@@ -1,21 +1,21 @@
-from base import BaseModel
+from peewee import TextField, IntegerField, ForeignKeyField
+from app.models.base import BaseModel
 from app.models.user import User
 from app.models.review_user import ReviewUser
 from app.models.review_place import ReviewPlace
-from peewee import TestField, IntegerField, ForeignKeyField
 
 class Review(BaseModel):
-    message = TextField(null = False)
-    stars = IntegerField(default = 0)
-    user = ForeignKeyField(User, related_name = 'reviews', on_delete = 'CASCADE')
+    message = TextField(null=False)
+    stars = IntegerField(default=0)
+    user = ForeignKeyField(User, related_name='reviews', on_delete='CASCADE')
 
-def to_hash(self):
-    user = User.get(User.id == self.user)
-    review = {
-        'from_user_id'  : user.id,
-        'to_user_id'    : ReviewUser,
-        'to_place_id'   : ReviewPlace,
-        'star'          : self.star,
-        'message'       : self.message
-        }
-    return super(Review, self).to_hash(self, review)
+    def to_hash(self):
+        user = User.get(User.id == self.user)
+        review = {
+            'from_user_id'  : user.id,
+            'to_user_id'    : ReviewUser,
+            'to_place_id'   : ReviewPlace,
+            'stars'          : self.stars,
+            'message'       : self.message
+            }
+        return super(Review, self).to_hash(self, review)
