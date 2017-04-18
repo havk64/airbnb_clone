e7ee31b583d857b9143bcbeb329504e7a3cf76b7
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-04-18 17:21:26 -0300

    Add Swagger to build api documentation

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_json import FlaskJSON
+from flasgger import Swagger
 
 app = Flask(__name__)
 app.config['JSON_ADD_STATUS'] = False
 app.config.update(
     PROPAGATE_EXCEPTIONS = True
 )
+Swagger(app)
 
 json = FlaskJSON(app)
 
